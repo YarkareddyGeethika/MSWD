47110d62dd002973bde53cf69f8ad247ce43e642
YarkareddyGeethika <74001247+YarkareddyGeethika@users.noreply.github.com>
2021-06-05 10:59:57 +0530

    Update App.js

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,99 +1,110 @@
-import React, { useState, useEffect } from 'react'
-import ContactList from './components/ContactList'
-import Filter from './components/Filter'
-import InputForm from './components/InputForm'
-import phoneNumberService from './services/phoneNumbers'
-import Notification from './components/Notification'
+import React from 'react'
 
+const Header = (props) => {
+  return (
+    <div>
+      <p>Name of course is: {props.course} </p>
+    </div>
+  )
+}
+
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>Name of part is: {props.p} and number of Exercises in this part: {props.ex} </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part =>
+        <Part p = {part.name} ex = {part.exercises} />
+      )}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts.reduce(function(sum, parts) { return sum + parts.exercises }, 0)
+  return (
+    <div>
+      <p>Total number of exercises are: {total} </p>
+    </div>
+  )
+}
+
+/* here we create the Course component as a separate module, which is
+   imported by the App component.*/
+const Course = (props) => {
+  return (
+    <div>
+      <h1> Web Development Curiculum </h1>
+      <Header course={props.course[0].name} />
+      <Content parts={props.course[0].parts} />
+      <Total parts={props.course[0].parts} />
+      <Header course={props.course[1].name} />
+      <Content parts={props.course[1].parts} />
+      <Total parts={props.course[1].parts} />
+    </div>
+  )
+}
 
 const App = () => {
 
-  const [contacts, setContacts] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState('')
-  const [notification, setNotification] = useState(null)
-
-  const handleFilterChange = (event) => { setNewFilter(event.target.value) }
-  const handleNameChange = (event) => { setNewName(event.target.value) }
-  const handleNumberChange = (event) => { setNewNumber(event.target.value) }
-
-  const hook = () => { phoneNumberService.getAll().then(response => setContacts(response.data)) }
-  useEffect(hook, [])
-
-  const messageDisplayTime = 3000 
-  const standardError = {type: 'errorNotification', message: 'Operation failed. Refresh your browser.'}
-  const deleteNotificationFunction = (name) => {
-    setNotification({type: 'deleteNotification', message: `Deleted ${name} from contacts!`})
-    setTimeout(() => {setNotification(null)}, messageDisplayTime)
-  }
-
-  const updateNotificationFunction = (name) => {
-    setNotification({type: 'updateNotification', message: `Updated ${name} in contacts!`})
-    setTimeout(() => {setNotification(null)}, messageDisplayTime)
-  }
-
-  const addNotificationFunction = (name) => {
-    setNotification({type: 'addNotification', message: `Added ${name} to contacts!`})
-    setTimeout(() => {setNotification(null)}, messageDisplayTime)
-  }
-
-  const errorNotificationFunction = () => {
-    setNotification(standardError)
-    setTimeout(() => {setNotification(null)}, messageDisplayTime)
-  }
-
-
-  const addContact = (event) => {
-
-    event.preventDefault()
-    const contactObject = { name: newName, number: newNumber }
-    const sameName = contacts.filter(contact => contact.name === newName)
-    if (sameName.length > 0) {
-      const msg = `Contact ${newName} is already in the phonebook. Do you want to replace the old contact?`
-      const confirm = window.confirm(msg)
-      if (confirm) {
-        phoneNumberService.update(sameName[0].id, contactObject).then(hook)
-        .then(() => {updateNotificationFunction(newName)}).catch(error => {errorNotificationFunction()})
-      }
-    } else {
-      phoneNumberService.create(contactObject).then(
-        response => {setContacts(contacts.concat(response.data))}
-      ).then(() => {addNotificationFunction(newName)})
-      .catch(error => {errorNotificationFunction()})
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
+  ]
 
-    setNewName('')
-    setNewNumber('')
-  }
-
-  const deleteContact = (event) => {
-    const button = event.target
-    const confirm = window.confirm(`Delete ${button.name}?`);
-    if (confirm) {
-      phoneNumberService.destroy(button.id).then(hook)
-      .then(() => {deleteNotificationFunction(button.name)})
-      .catch(error => {errorNotificationFunction()})
-    }
-  }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Notification notification={notification}></Notification>
-      <Filter value={newFilter} onChange={handleFilterChange}></Filter>
-
-      <h2>Add new</h2>
-      <InputForm
-        addObject={addContact} newName={newName} handleNameChange={handleNameChange}
-        newNumber={newNumber} handleNumberChange={handleNumberChange}>
-      </InputForm>
-
-      <h2>Numbers</h2>
-      <ContactList contacts={contacts} filter={newFilter} deleteFun={deleteContact}></ContactList>
+      <Course course={course} />
     </div>
   )
-
 }
 
 export default App
